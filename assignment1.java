b5a40d837ba0c4d7a2292257ffc8c0db3ff725aa
pontusnorman123 <pontusnorman1@gmail.com>
2023-01-27 14:19:38 +0100

    feat(#1): Preliminary skeleton code for decide function, Required computions assignment section, PUM section and FUV section.

--- assignment1.java
+++ assignment1.java
@@ -3,7 +3,70 @@
  */
 public class assignment1 {
 
+    public class PARAMETERS
+    {
+        double LENGTH1;
+        double RADIUS1;
+        double EPSILON;
+        double AREA1;
+        int Q_PTS;
+        int QUADS;
+        double DIST;
+        int N_PTS;
+        int K_PTS;
+        int A_PTS;
+        int B_PTS;
+        int C_PTS;
+        int D_PTS;
+        int E_PTS;
+        int F_PTS;
+        int G_PTS;
+        double RADIUS2;
+        double LENGTH2;
+        double AREA2;
+    }
+
+
+    public static int[] CMV()
+    {
+        int[] CMV;
+
+
+
+        return CMV;
+    }
+
+    public static int[][] PUM(int[] CVM, int[][] LCM)
+    {
+        int[][] PUM;
+        return PUM;
+    }
+
+    public static int[] FUV(int[][] PUM)
+    {
+        int[] FUV;
+        return FUV;
+    }
+    public static void decide(int NUMPOINTS, int [][] POINTS, PARAMETERS Parameters, int[][] LCM, int[] PUV)
+    {
+
+        boolean launch = false;
+
+        int[] CMV = CMV();
+        int[][] PUM = PUM(CMV,LCM);
+        int[] FUV = FUV(PUM);
+
+
+        if(FUV)
+        {
+            launch = true;
+        }
+
+
+    }
     public static void main(String[] args) {
-        
+
+
+        decide()
     }
 }
