089c4325f1ab54231df07b478652e402b733a8c2
pontusnorman123 <pontusnorman1@gmail.com>
2023-01-27 15:07:14 +0100

    feat(#1): Updated CMV function. The current plan is to use a switch case for each condition

--- assignment1.java
+++ assignment1.java
@@ -26,33 +26,78 @@ public class assignment1 {
         double AREA2;
     }
 
-
-    public static int[] CMV()
+    public static boolean calc_CMV(PARAMETERS Parameters, int cond) // Return true or false depending on if condition checks out
     {
-        int[] CMV;
+        switch (cond){
+            case 0:
+
+            case 1:
+
+            case 2:
+
+            case 3:
+
+            case 4:
+
+            case 5:
+
+            case 6:
+
+            case 7:
+
+            case 8:
+
+            case 9:
+
+            case 10:
+
+            case 11:
+
+            case 12:
+
+            case 13:
+
+            case 14:
 
 
+
+        }
+
+    }
+
+    public static int[] CMV(PARAMETERS Parameters)
+    {
+        int[] CMV = new int[15];
+
+        for(int i = 0; i < 15; i++)
+        {
+            CMV[i] = calc_CMV(Parameters,i);
+        }
 
         return CMV;
     }
 
-    public static int[][] PUM(int[] CVM, int[][] LCM)
+    public static int[][] PUM(int[] CVM, int[][] LCM) // PRELIMINARY
     {
-        int[][] PUM;
+        int[][] PUM = new int[0][0];
         return PUM;
     }
 
-    public static int[] FUV(int[][] PUM)
+    public static int[] FUV(int[][] PUM) // PRELIMINARY
     {
-        int[] FUV;
+        int[] FUV = new int[0];
         return FUV;
     }
     public static void decide(int NUMPOINTS, int [][] POINTS, PARAMETERS Parameters, int[][] LCM, int[] PUV)
     {
 
+        /*
+        Preliminary plan of how it should work
+        first calculate CMV then PUM and then FUV and if FUV is approved send launch approve
+
         boolean launch = false;
 
-        int[] CMV = CMV();
+        int[] CMV = CMV(PARAMETERS Parameters);
         int[][] PUM = PUM(CMV,LCM);
         int[] FUV = FUV(PUM);
 
@@ -62,11 +107,16 @@ public class assignment1 {
             launch = true;
         }
 
+         */
+
+
+
 
     }
     public static void main(String[] args) {
 
 
-        decide()
+        // here the decide function should be run
+        // decide()
     }
 }
