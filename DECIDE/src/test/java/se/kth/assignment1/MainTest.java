c95bae646416ec8462d018581ed9c8c825a6aad5
Porsev Aslan <porsev.aslan@tink.com>
2023-01-28 16:07:29 +0100

    feat(Gradle): Add Gradle build files and configurations

---
+++ DECIDE/src/test/java/se/kth/assignment1/MainTest.java
@@ -0,0 +1,22 @@
+package se.kth.assignment1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
