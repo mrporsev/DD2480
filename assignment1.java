ac653a931016d950046d3c0d2192b2447adf1ad4
David Cederström <davidced@kth.se>
2023-01-28 19:10:10 +0100

    feat(#10): Added simple logic for LIC2. Takes three data points, calculates distance between them and then calculates the angle of the vertex in the second data point.

--- assignment1.java
+++ assignment1.java
@@ -36,12 +36,14 @@ public class assignment1 {
             case 0:
 
             case 1:
-                // There exists at least one set of three consecutive data points that cannot
-                // all be contained
-                // within or on a circle of radius RADIUS1.
-                // (0 ≤ RADIUS1)
-                // The same as saying that the distance between a point and every other can't be
-                // bigger than the diameter?
+                /*
+                 * There exists at least one set of three consecutive data points that cannot
+                 * all be contained
+                 * within or on a circle of radius RADIUS1.
+                 * (0 ≤ RADIUS1)
+                 * The same as saying that the distance between a point and every other can't be
+                 * bigger than the diameter?
+                 */
 
                 double diameter = 2 * RADIUS1;
                 int[] pointA;
@@ -54,33 +56,36 @@ public class assignment1 {
                     pointB = listOfCoordinates[i + 1];
                     pointC = listOfCoordinates[i + 2];
 
-                    // Length between x-coordinates
+                    // Length between x-coordinates to...
+                    // Point A
                     int xDistanceA1 = Math.abs(pointA[0] - pointB[0]);
                     int xDistanceA2 = Math.abs(pointA[0] - pointC[0]);
-
+                    // Point B
                     int xDistanceB1 = Math.abs(pointB[0] - pointA[0]);
                     int xDistanceB2 = Math.abs(pointB[0] - pointC[0]);
-
+                    // Point C
                     int xDistanceC1 = Math.abs(pointC[0] - pointA[0]);
                     int xDistanceC2 = Math.abs(pointC[0] - pointB[0]);
 
-                    // Length between y-coordinates
+                    // Length between y-coordinates to...
+                    // Point A
                     int yDistanceA1 = Math.abs(pointA[1] - pointB[1]);
                     int yDistanceA2 = Math.abs(pointA[1] - pointC[1]);
-
+                    // Point B
                     int yDistanceB1 = Math.abs(pointB[1] - pointA[1]);
                     int yDistanceB2 = Math.abs(pointB[1] - pointC[1]);
-
+                    // Point C
                     int yDistanceC1 = Math.abs(pointC[1] - pointA[1]);
                     int yDistanceC2 = Math.abs(pointC[1] - pointB[1]);
 
                     // The length between the points
+                    // From B & C to A
                     double distanceA1 = Math.sqrt((xDistanceA1 * xDistanceA1) + (yDistanceA1 * yDistanceA1));
                     double distanceA2 = Math.sqrt((xDistanceA2 * xDistanceA2) + (yDistanceA2 * yDistanceA2));
-
+                    // From A & C to B
                     double distanceB1 = Math.sqrt((xDistanceB1 * xDistanceB1) + (yDistanceB1 * yDistanceB1));
                     double distanceB2 = Math.sqrt((xDistanceB2 * xDistanceB2) + (yDistanceB2 * yDistanceB2));
-
+                    // From A & B to C
                     double distanceC1 = Math.sqrt((xDistanceC1 * xDistanceC1) + (yDistanceC1 * yDistanceC1));
                     double distanceC2 = Math.sqrt((xDistanceC2 * xDistanceC2) + (yDistanceC2 * yDistanceC2));
 
@@ -94,6 +99,62 @@ public class assignment1 {
                 }
 
             case 2:
+                /*
+                 * There exists at least one set of three consecutive data points which form an
+                 * angle such that:
+                 * angle < (PI−EPSILON)
+                 * or
+                 * angle > (PI+EPSILON)
+                 * The second of the three consecutive points is always the vertex of the angle.
+                 * If either the first point or the last point (or both) coincides with the
+                 * vertex,
+                 * the angle is undefined and the LIC is not satisfied by those three points.
+                 * (0 ≤ EPSILON < PI)
+                 */
+
+                int[] pointA;
+                int[] pointB;
+                int[] pointC;
+
+                for (int i = 0; i < listOfCoordinates.length - 2; i++) {
+
+                    pointA = listOfCoordinates[i];
+                    pointB = listOfCoordinates[i + 1]; // The vertex
+                    pointC = listOfCoordinates[i + 2];
+
+                    // Length between x-coordinates to...
+                    // Point A
+                    int xDistanceAB = Math.abs(pointA[0] - pointB[0]);
+                    // Point B
+                    int xDistanceBC = Math.abs(pointB[0] - pointC[0]);
+                    // Point C
+                    int xDistanceCA = Math.abs(pointC[0] - pointA[0]);
+
+                    // Length between y-coordinates to...
+                    // Point A
+                    int yDistanceAB = Math.abs(pointA[1] - pointB[1]);
+                    // Point B
+                    int yDistanceBC = Math.abs(pointB[1] - pointC[1]);
+                    // Point C
+                    int yDistanceCA = Math.abs(pointC[1] - pointA[1]);
+
+                    // The length between the points
+                    double distanceAB = Math.sqrt((xDistanceAB * xDistanceAB) + (yDistanceAB * yDistanceAB)); // A - B
+                    double distanceBC = Math.sqrt((xDistanceBC * xDistanceBC) + (yDistanceBC * yDistanceBC)); // B - C
+                    double distanceCA = Math.sqrt((xDistanceCA * xDistanceCA) + (yDistanceCA * yDistanceCA)); // C - A
+
+                    // The angle of the second point (B)
+                    double angle = Math
+                            .acos(((distanceAB * distanceAB) + (distanceBC * distanceBC) + (distanceCA * distanceCA))
+                                    / (2 * distanceAB * distanceBC));
+
+                    if ((angle < (Math.PI - EPSILON)) || (angle > (Math.PI + EPSILON))) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+
+                }
 
             case 3:
 
