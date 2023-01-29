d7deac5658d1b921949eb8c32a8e8f10de9dea3d
pontusnorman123 <pontusnorman1@gmail.com>
2023-01-29 20:43:37 +0100

    feat(#15): Implemented CMV 7 and utility function points_inbetween() and length_between_points()
    style(#4): changed the method to use the utility functions

--- assignment1.java
+++ assignment1.java
@@ -1,7 +1,9 @@
 /**
  * assignment1
  */
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.lang.Math;
 
@@ -10,6 +12,7 @@ public class assignment1 {
 
     public class POINT   // One point class
     {
+
 
         POINT(double x, double y)
         {
@@ -78,7 +81,7 @@ public class assignment1 {
 
     public class PARAMETERS
     {
-        double LENGTH1 = 15;
+        double LENGTH1 = 0;
         double RADIUS1;
         double EPSILON;
         double AREA1;
@@ -99,37 +102,93 @@ public class assignment1 {
         double AREA2;
     }
 
+    /**
+     *
+     * @param a data point a
+     * @param b data point b
+     * @return lenght between a and b
+     */
+    double lengt_between_points(POINT a, POINT b)
+    {
+
+        double x_a = a.getX();
+        double y_a = a.getY();
+        double x_b = b.getX();
+        double y_b = b.getY();
+
+        double dis_x = Math.abs(x_a - x_b);
+        double dis_y = Math.abs(y_a - y_b);
+        double tot_dis = Math.sqrt(Math.pow(dis_x,2) + Math.pow(dis_y,2)); // d=√((x2 – x1)² + (y2 – y1)²). formula
+
+        return tot_dis;
+    }
+
+    /**
+     *
+     * @param a data point a
+     * @param b data point b
+     * @param arr array of data points
+     * @return array of datapoints that lie between a and b
+     */
+    List<POINT> points_inbetween(POINT a, POINT b, POINT[] arr) //  Checks if there exists any points in the list c inbetween two points a and b.
+    {
+        double x_a = a.getX();
+        double y_a = a.getY();
+        double x_b = b.getX();
+        double y_b = b.getY();
+        POINT c;
+
+        double dist_a_c = 0;
+        double dist_b_c = 0;
+        double dist_a_b = lengt_between_points(a,b);
+        List<POINT> res = new ArrayList<POINT>();
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            c = arr[i];
+
+            if(c.equals(a) || c.equals(b))
+            {
+                break;
+            }
+
+            dist_a_c = lengt_between_points(a,c);
+            dist_b_c = lengt_between_points(b,c);
+
+            if(dist_a_c + dist_b_c == dist_a_b)
+            {
+                res.add(c);
+            }
+
+        }
+        return res;
+    }
+
     public boolean calc_CMV(int cond) // Return true or false depending on if condition checks out
     {
         PARAMETERS Parameters = new PARAMETERS();
         Points Points = new Points(Globals.NUMPOINTS);
 
         switch (cond){
-            case 0:
+            case 0: // Kinda tested
 
-                double cur_x = 0;
-                double cur_y = 0;
-                double comp_x = 0;
-                double comp_y = 0;
-                double dis_x = 0;
-                double dis_y = 0;
-                double tot_dis = 0;
 
+                double dis = 0;
+
+                POINT cur_point;
+                POINT next_point;
 
                 for(int i = 0; i < Globals.NUMPOINTS; i++)
                 {
-                    cur_x = Points.POINTS[i].getX();
-                    cur_y = Points.POINTS[i].getY();
+
+                    cur_point = Points.POINTS[i];
 
                     for(int j = 0; j < Globals.NUMPOINTS; j++)
                     {
-                        comp_x = Points.POINTS[j].getX();
-                        comp_y = Points.POINTS[j].getY();
-                        dis_x = Math.abs(cur_x - comp_x);
-                        dis_y = Math.abs(cur_y - comp_y);
-                        tot_dis = Math.sqrt(Math.pow(dis_x,2) + Math.pow(dis_y,2)); // d=√((x2 – x1)² + (y2 – y1)²). formula
+                        next_point = Points.POINTS[j];
+                        dis = lengt_between_points(cur_point,next_point);
 
-                        if(tot_dis > Parameters.LENGTH1)
+                        if(dis > Parameters.LENGTH1)
                         {
                             return false;
                         }
@@ -150,7 +209,49 @@ public class assignment1 {
 
             case 6:
 
-            case 7:
+            case 7: // Untested
+
+                if(Globals.NUMPOINTS < 3)
+                {
+                    return false;
+                }
+
+                POINT p1;
+                POINT p2;
+                List<POINT> points_between;
+                POINT k1;
+                POINT k2;
+                double dis1;
+
+
+
+                for(int i = 0; i < Globals.NUMPOINTS - 1; i++)
+                {
+                    p1 = Points.POINTS[i];
+                    p2 = Points.POINTS[i+1];
+                    points_between = points_inbetween(p1,p2,Points.POINTS);
+
+                    if(points_between.size() == Parameters.K_PTS)
+                    {
+                        for(int j = 0; j < points_between.size(); j++)
+                        {
+                            k1 = points_between.get(j);
+
+                                    for(int k = 0; k < points_between.size(); k++)
+                                    {
+                                        k2 = points_between.get(k);
+                                        dis1 = lengt_between_points(k1,k2);
+                                        if(dis1 <= Parameters.LENGTH1 && !k1.equals(k2))
+                                        {
+                                            return false;
+                                        }
+                                    }
+                        }
+                    }
+
+                }
+                return true;
+
 
             case 8:
 
@@ -229,6 +330,7 @@ public class assignment1 {
 
 
         assignment1 assignment1 = new assignment1();
+        System.out.println(assignment1.calc_CMV(7));
 
 
     }
