df459384894edd15a426e4fc03202d63df4f25eb
pontusnorman123 <pontusnorman1@gmail.com>
2023-01-29 09:34:31 +0100

    feat(#8): Point and Points class to read coordinates from. See calc_CMV case 0 for example feat(#4): Implemented CMV 0 and a Global class which NUMPOINTS can be read from

--- assignment1.java
+++ assignment1.java
@@ -1,11 +1,84 @@
 /**
  * assignment1
  */
+import java.util.Arrays;
+import java.util.Random;
+import java.lang.Math;
+
+
 public class assignment1 {
+
+    public class POINT   // One point class
+    {
+
+        POINT(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        double x;
+        double y;
+
+        double getX()
+        {
+            return this.x;
+        }
+
+        double getY()
+        {
+            return this.y;
+        }
+    }
+
+    public class Points
+    {
+        public Points(int size) // Constructs Points array with random numbers
+        {
+            Random rand = new Random();
+            double upperbound = 10.0;
+            double double_random = rand.nextDouble(upperbound);
+            this.POINTS = new POINT[size];
+            for(int i = 0; i < size; i++)
+            {
+                POINT point = new POINT(double_random,double_random);
+                this.POINTS[i] = point;
+                double_random = rand.nextDouble(upperbound);
+
+            }
+        }
+
+        public Points(int size, double val) // Constructs Points array with val as coordinates
+        {
+            for(int i = 0; i < size; i++)
+            {
+                this.POINTS = new POINT[size];
+                POINT point = new POINT(val,val);
+                this.POINTS[i] = point;
+            }
+
+        }
+
+        POINT[] getPOINTS()
+        {
+            return this.POINTS;
+        }
+
+
+
+        POINT[] POINTS;
+    }
+    public class Globals   // Global class that stores global variables
+    {
+
+        public static int NUMPOINTS = 10;
+
+
+
+    }
 
     public class PARAMETERS
     {
-        double LENGTH1;
+        double LENGTH1 = 15;
         double RADIUS1;
         double EPSILON;
         double AREA1;
@@ -26,10 +99,44 @@ public class assignment1 {
         double AREA2;
     }
 
-    public static boolean calc_CMV(PARAMETERS Parameters, int cond) // Return true or false depending on if condition checks out
+    public boolean calc_CMV(int cond) // Return true or false depending on if condition checks out
     {
+        PARAMETERS Parameters = new PARAMETERS();
+        Points Points = new Points(Globals.NUMPOINTS);
+
         switch (cond){
             case 0:
+
+                double cur_x = 0;
+                double cur_y = 0;
+                double comp_x = 0;
+                double comp_y = 0;
+                double dis_x = 0;
+                double dis_y = 0;
+                double tot_dis = 0;
+
+
+                for(int i = 0; i < Globals.NUMPOINTS; i++)
+                {
+                    cur_x = Points.POINTS[i].getX();
+                    cur_y = Points.POINTS[i].getY();
+
+                    for(int j = 0; j < Globals.NUMPOINTS; j++)
+                    {
+                        comp_x = Points.POINTS[j].getX();
+                        comp_y = Points.POINTS[j].getY();
+                        dis_x = Math.abs(cur_x - comp_x);
+                        dis_y = Math.abs(cur_y - comp_y);
+                        tot_dis = Math.sqrt(Math.pow(dis_x,2) + Math.pow(dis_y,2)); // d=√((x2 – x1)² + (y2 – y1)²). formula
+
+                        if(tot_dis > Parameters.LENGTH1)
+                        {
+                            return false;
+                        }
+
+                    }
+                }
+                return true;
 
             case 1:
 
@@ -63,15 +170,16 @@ public class assignment1 {
 
         }
 
+        return false;
     }
 
-    public static int[] CMV(PARAMETERS Parameters)
+    public boolean[] CMV(PARAMETERS Parameters)
     {
-        int[] CMV = new int[15];
+        boolean[] CMV = new boolean[15];
 
         for(int i = 0; i < 15; i++)
         {
-            CMV[i] = calc_CMV(Parameters,i);
+            CMV[i] = calc_CMV(i);
         }
 
         return CMV;
@@ -118,5 +226,10 @@ public class assignment1 {
 
         // here the decide function should be run
         // decide()
+
+
+        assignment1 assignment1 = new assignment1();
+
+
     }
 }
