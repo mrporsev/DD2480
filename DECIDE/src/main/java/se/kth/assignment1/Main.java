0849d339f075f953909224099b6db231114b7e01
Porsev Aslan <porsev.aslan@tink.com>
2023-01-30 00:42:25 +0100

    feat(Decide): Added skeleton code for the decide function #20
    In this commit I've added the skeleton for the decide function
    as well as creating some of the objects necessary as inputs for
    the decide function.

--- DECIDE/src/main/java/se/kth/assignment1/Main.java
+++ DECIDE/src/main/java/se/kth/assignment1/Main.java
@@ -1,7 +1,35 @@
 package se.kth.assignment1;
 
 public class Main {
+
+    /**
+     * Decide function skeleton
+     * @param numpoints The number of planar data points.
+     * @param points Array containing the coordinates of data points.
+     * @param parameters Object holding parameters for LIC's.
+     * @param LCM Logical Connector Matrix.
+     * @param PUV Preliminary Unlocking Vector
+     */
+    public static void decide(int numpoints, Points points, Parameters parameters, int[][] LCM, boolean[] PUV) {
+
+        //CMV cmv = new CMV(parameters);
+        //PUM pum = new PUM(LCM, cmv);
+        //FUV fuv = new FUV(pum, PUV);
+
+        //Return LAUNCH ("Yes", "No"), CMV, PUM, FUV
+    }
+    /**
+     * This function does this
+     * @param args argument is this
+     */
     public static void main(String[] args) {
 
+        Parameters parameters = new Parameters();
+        int numpoints = 10;
+        Points points = new Points(numpoints);
+        int[][] LCM = new int[15][15];
+        boolean[] PUV = new boolean[15];
+
+        decide(numpoints, points, parameters, LCM, PUV);
     }
 }
