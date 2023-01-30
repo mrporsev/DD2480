117fb5d25f91ccb1a38476610baa50d96f06c225
Adam Ezzaim <ezzaim@kth.se>
2023-01-30 10:40:21 +0100

    feat(PUM): setup of Pum class #31
    Setup of Pum class with get methods.
    I assumed that we have a get() method in Cmv class that returns boolean[] cmv.

--- DECIDE/src/main/java/se/kth/assignment1/Pum.java
+++ DECIDE/src/main/java/se/kth/assignment1/Pum.java
@@ -1,4 +1,48 @@
 package se.kth.assignment1;
 
+/**
+ * Preliminary Unlocking Matrix
+ */
 public class Pum {
+
+    private boolean[][] pum = new boolean[15][15];
+
+    /**
+     * @param LCM Logical Connector Matrix
+     *            15x15 int Matrix with the following mapping
+     *            0 -> Not used
+     *            1 -> and
+     *            2 -> or
+     * @param CMV Conditions Met Vector
+     */
+    public Pum(int[][] LCM, Cmv CMV) {
+        boolean[] cmv = CMV.get();
+
+        for (int i = 0; i < 15 ; i++) {
+            for (int j = 0; j <= i; j++) {
+                switch (LCM[i][j]) {
+                    case 0 : pum[i][j] = true;break;
+                    case 1 : pum[i][j] = cmv[i] && cmv[j];break;
+                    case 2 : pum[i][j] = cmv[i] || cmv[j];break;
+                }
+                pum[i][j] = pum[j][i];
+            }
+        }
+    }
+
+    /**
+     * @return Preliminary Unlocking Matrix
+     */
+    public boolean[][] getPum() {
+        return pum;
+    }
+
+    /**
+     * @param i
+     * @param j
+     * @return field [i][j] of Preliminary Unlocking Matrix
+     */
+    public boolean getElem(int i, int j) {
+        return pum[i][j];
+    }
 }
