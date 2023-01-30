1eabd0ab361839cbd6d77ad27be04acd7c08c870
David Cederström <davidced@kth.se>
2023-01-31 00:16:44 +0100

    test(LIC): Creating a test class for the LICs #49.

---
+++ DECIDE/src/test/java/se/kth/assignment1/LICTest.java
@@ -0,0 +1,82 @@
+package se.kth.assignment1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LICTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void cond0() {
+
+    }
+
+    @Test
+    void cond1() {
+    
+    }
+
+    @Test
+    void cond2() {
+    }
+
+    @Test
+    void cond3() {
+    }
+
+    @Test
+    void cond4() {
+    }
+
+    @Test
+    void cond5() {
+    }
+
+    @Test
+    void cond6() {
+    }
+
+    @Test
+    void cond7() {
+    }
+
+    @Test
+    void cond8() {
+    }
+
+    @Test
+    void cond9() {
+    }
+
+    @Test
+    void cond10() {
+    }
+
+    @Test
+    void cond11() {
+    }
+
+    @Test
+    void cond12() {
+    }
+
+    @Test
+    void cond13() {
+    }
+
+    @Test
+    void cond14() {
+    }
+}
