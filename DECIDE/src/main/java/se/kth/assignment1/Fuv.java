4eb4f5a1109850f34522e7976aff3db151c14886
Adam Ezzaim <adam.ezzaim@epfl.ch>
2023-01-30 12:12:21 +0100

    feat(FUV): setup of Fuv class #33
    Setup of Fuv class with get methods.
    I assumed that we have a getPum() method in Pum class that returns boolean[][] pum.

--- DECIDE/src/main/java/se/kth/assignment1/Fuv.java
+++ DECIDE/src/main/java/se/kth/assignment1/Fuv.java
@@ -1,4 +1,30 @@
 package se.kth.assignment1;
 
 public class Fuv {
+
+    private boolean[] fuv = new boolean[15];
+
+    public Fuv(Pum pum, boolean[] PUV){
+        Pum PUM = pum.getPum();
+
+        for (int i = 0; i < 15; i++) {
+            if (!PUV[i]) fuv[i] = true;
+            else {
+                fuv[i] = true;
+                for (int j = 0; j < 15 ; j++) {
+                    if(j != i && !PUM[i][j] ) {
+                        fuv[i] = false;
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean[] getFuv() {
+        return fuv;
+    }
+
+    public boolean[] getElem(int i) {
+        return fuv[i];
+    }
 }
